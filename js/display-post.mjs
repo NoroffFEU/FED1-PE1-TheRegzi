fb888efc520565516fafc08789c537115f05da7a
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-05-06 12:48:48 +0200

    updated and removed unnecessary js file

--- js/display-post.mjs
+++ js/display-post.mjs
@@ -1,5 +1,29 @@
 
-import { fetchPosts } from './fetch-post.mjs';
+
+export async function fetchPosts() {
+    const name = localStorage.getItem('name');
+    const apiUrl = `https://v2.api.noroff.dev/blog/posts/${name}`;
+    const userToken = localStorage.getItem('userToken'); 
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${userToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const posts = await response.json();
+        return posts.data;
+    } catch (error) {
+        console.error('Failed to fetch posts:', error);
+    }
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
